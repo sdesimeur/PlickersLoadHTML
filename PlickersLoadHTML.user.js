62a685118e3d47036b076436d92f15bb188b4168
sdesimeur <pro@sdesimeur.com>
2017-10-13 18:14:39 +0200

    test (p20)

--- PlickersLoadHTML.user.js
+++ PlickersLoadHTML.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PlickersLoadHTML
 // @namespace    http://sdesimeur.com/
-// @version      1.4
+// @version      1.5
 // @description  try to take over the world!
 // @author       SDesimeur
 // @include https://plickers.com/*
