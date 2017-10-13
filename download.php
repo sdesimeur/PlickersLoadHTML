640b426e2960c3f7594ea7271d2e22af884d51e1
sdesimeur <pro@sdesimeur.com>
2017-10-13 17:31:42 +0200

    test (p16)

--- download.php
+++ download.php
@@ -1,6 +1,14 @@
 <?php
     header('Content-type: text/plain');
     $url=base64_decode($_POST['url']);
-    $content=file_get_contents($url);
+	$ch = curl_init();
+	$timeout = 5;
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	$content = curl_exec($ch);
+	curl_close($ch);
+
+    //$content=file_get_contents($url);
     echo($content);
 ?>
