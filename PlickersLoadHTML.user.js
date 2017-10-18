e0dcd082a29660601e42e5943237692320259c65
sdesimeur <pro@sdesimeur.com>
2017-10-18 19:58:17 +0200

    add sections loading file (debug 14)

--- PlickersLoadHTML.user.js
+++ PlickersLoadHTML.user.js
@@ -1,7 +1,7 @@
 //==UserScript==
 // @name		 PlickersLoadHTML
 // @namespace	http://sdesimeur.com/
-// @version	  1.42
+// @version	  1.43
 // @description  try to take over the world!
 // @author	SDesimeur
 // @include https://plickers.com/*
@@ -47,7 +47,7 @@ function changeItemByHTML (questionDiv,questionSec) {
 		}
 
 		if (questionSec!==null) {
-			var currentVM=angular.element(questionSec.querySelector('pl-library-question')).scope().vm;
+			var currentVM=angular.element(questionDiv).scope().vm;
 			if (! questionDiv.classList.contains('trueResponsesChanged')) {
 				var oReq2=new XMLHttpRequest();
 				//oReq.open("GET", url4Download + btoa(tmpURL+"/Question.html"), true);
