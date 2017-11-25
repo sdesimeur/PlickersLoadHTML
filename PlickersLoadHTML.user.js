b632b4d84aa019fe9e3f320903c7a4fdceef4f68
sdesimeur <pro@sdesimeur.com>
2017-11-25 11:35:05 +0100

    change version num

--- PlickersLoadHTML.user.js
+++ PlickersLoadHTML.user.js
@@ -1,7 +1,7 @@
 //==UserScript==
 // @name		 PlickersLoadHTML
 // @namespace	http://sdesimeur.com/
-// @version	  1.54
+// @version	  1.55
 // @description  try to take over the world!
 // @author	SDesimeur
 // @include https://plickers.com/*
